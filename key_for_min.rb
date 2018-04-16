c9cdf21aa4587496e1df6d6be4c6dce9a5d53298
Tara Ellicott <taraellicott@gmail.com>
2018-04-16 16:41:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,15 +12,3 @@ def key_for_min_value(name_hash)
   end
   min_key
 end
-
-# def key_for_min_value(hash)
-#   lowest_key = nil
-#   lowest_value = nil
-#   hash.each do |k, v|
-#     if lowest_value == nil || v < lowest_value
-#       lowest_value = v
-#       lowest_key = k
-#     end
-#   end
-#   lowest_key
-# end
